255e90c6992073222fa84265cb8e2ef3b7cc0829
abbatrombone <redggreen@gmail.com>
2025-12-20 13:04:42 -0600

    Made Construction Better

--- src/main/java/me/abbatrombone/traz/GameItems/Stratagems.java
+++ src/main/java/me/abbatrombone/traz/GameItems/Stratagems.java
@@ -1,51 +1,58 @@
 package me.abbatrombone.traz.GameItems;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class Stratagems {
-    private final StaragemColor staragemColor;
     private final String stragemName;
+    private final StratagemColor staragemColor;
+    private final String subtype;
 
-    enum StaragemColor{
-        Red("Eagle","Orbital","N/A","N/A","N/A"),
-        Blue("Weapon","Backpack","Weapon With Backpack","Mech","Vehicle"),
-        Green("Turret","Mines","Encampment","N/A","N/A");
+    public enum Subtype {
+        EAGLE, ORBITAL,
+        WEAPON, BACKPACK, WEAPON_WITH_BACKPACK, MECH, VEHICLE,
+        TURRET, MINES, ENCAMPMENT
+    }
 
-        private final String subType1;
-        private final String subType2;
-        private final String subType3;
-        private final String subType4;
-        private final String subType5;
+    public enum StratagemColor {
+        RED(EnumSet.of(Subtype.EAGLE, Subtype.ORBITAL)),
+        BLUE(EnumSet.of(Subtype.WEAPON, Subtype.BACKPACK, Subtype.WEAPON_WITH_BACKPACK, Subtype.MECH, Subtype.VEHICLE)),
+        GREEN(EnumSet.of(Subtype.TURRET, Subtype.MINES, Subtype.ENCAMPMENT));
 
-        private StaragemColor(String subType1,String subType2,String subType3,String subType4,String subType5){
-            this.subType1 = subType1;
-            this.subType2 = subType2;
-            this.subType3 = subType3;
-            this.subType4 = subType4;
-            this.subType5 = subType5;
+        private final Set<Subtype> subTypes;
+
+        StratagemColor(Set<Subtype> subTypes) {
+            this.subTypes = subTypes;
         }
 
-        public String getSubType1() {
-            return subType1;
-        }
-
-        public String getSubType2() {
-            return subType2;
-        }
-
-        public String getSubType3() {
-            return subType3;
-        }
-
-        public String getSubType4() {
-            return subType4;
-        }
-
-        public String getSubType5() {
-            return subType5;
+        public boolean allows(Subtype subtype) {
+            return subTypes.contains(subtype);
         }
     }
 
-    public Stratagems(String stragemName,StaragemColor staragemColor){
-        this.stragemName = stragemName;
-        this.staragemColor = staragemColor;
+//   public Stratagems(String stragemName, StratagemColor staragemColor, String subtype){
+//       this.stragemName = stragemName;
+//       this.staragemColor = staragemColor;
+//       this.subtype = subtype;
+//   }
+
+    public Stratagems(String name, StratagemColor color, Subtype subtype) {
+        if (!color.allows(subtype)) {
+            throw new IllegalArgumentException(
+                    "Invalid subtype " + subtype + " for color " + color
+            );
+        }
+        this.stragemName = name;
+        this.staragemColor = color;
+        this.subtype = String.valueOf(subtype);
     }
+
+    @Override
+    public String toString() {
+        return "Stratgem{" +
+                "color=" + staragemColor +
+                ", subtype='" + subtype + '\'' +
+                '}';
+    }
+
 }
