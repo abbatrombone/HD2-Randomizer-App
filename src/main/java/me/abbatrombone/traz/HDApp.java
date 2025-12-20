746f81fb02e4dce4585add418f56553e40226a83
abbatrombone <redggreen@gmail.com>
2025-12-20 13:06:56 -0600

    Removed unneeded params

--- src/main/java/me/abbatrombone/traz/HDApp.java
+++ src/main/java/me/abbatrombone/traz/HDApp.java
@@ -1,19 +1,12 @@
 package me.abbatrombone.traz;
 
-import me.abbatrombone.traz.CustomComponents.OutputJTextPane;
 import me.abbatrombone.traz.Panels.*;
-
 import javax.swing.*;
 
 public class HDApp {
     private final JFrame frame = new JFrame();
 
-    private final LabelPanel labelPanel = new LabelPanel();
-    private final JPanel labelPanelFull = new JPanel();
-    private OutputJTextPane output = new OutputJTextPane();
-    JTabbedPane tabPanel = new JTabbedPane();
-    private final SelectBondsPanel selectBondsPanel = new SelectBondsPanel();
-    private final ButtonPanel buttonPanel = new ButtonPanel(output);
+    private final JTabbedPane tabPanel = new JTabbedPane();
     private final MainPanel mainPanel = new MainPanel();
     private final GearPanel gearPanel = new GearPanel();
     private final RulesPanel rulesPanel = new RulesPanel();
