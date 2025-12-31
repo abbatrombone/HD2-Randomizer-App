dc4aaed570411997ec8634bd686634685558cd4e
abbatrombone <redggreen@gmail.com>
2025-12-30 22:17:49 -0600

    Test

--- src/main/java/me/abbatrombone/traz/Main.java
+++ src/main/java/me/abbatrombone/traz/Main.java
@@ -6,8 +6,32 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("TEst");
         LoggerSetup.initializeLogger();
         SwingUtilities.invokeLater(HDApp::new);
+//        feel();
+//        SwingUtilities.invokeLater( () -> {
+//            new HDApp();
+//        } );
+//    }
+//
+//    static void feel() {
+//        try {
+//            for( javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels() ) {
+//                if( "Nimbus".equals( info.getName() ) ) {
+//                    javax.swing.UIManager.setLookAndFeel( info.getClassName() );
+//                    break;
+//
+//                }
+//            }
+//        }
+//        catch( ClassNotFoundException | InstantiationException
+//               | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex ) {
+//            java.util.logging.Logger.getLogger( HDApp.class
+//                            .getName() )
+//                    .log( java.util.logging.Level.SEVERE, null, ex );
+//        }
+//
     }
 }
 
