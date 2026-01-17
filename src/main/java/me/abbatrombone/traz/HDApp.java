976e84686daa87554448cd19efc4e8e822290994
abbatrombone <redggreen@gmail.com>
2026-01-16 23:59:25 -0600

    Adds HD2 cursor to Panel

--- src/main/java/me/abbatrombone/traz/HDApp.java
+++ src/main/java/me/abbatrombone/traz/HDApp.java
@@ -1,20 +1,42 @@
 package me.abbatrombone.traz;
 
+import me.abbatrombone.traz.CustomComponents.CustomCursor;
 import me.abbatrombone.traz.CustomComponents.TabPane.MyComponent;
 import me.abbatrombone.traz.CustomComponents.TabPane.MyTabbedPane;
 import me.abbatrombone.traz.Panels.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HDApp {
+    private static final Logger logger = Logger.getLogger(HDApp.class.getName());
+
     private final JFrame frame = new JFrame();
-    private final JTabbedPane tabPanel = makeTabPane();
     private final MainPanel mainPanel = new MainPanel();
-    private final GearPanel gearPanel = new GearPanel();
     private final RulesPanel rulesPanel = new RulesPanel();
+    private final SettingsPanel settingsPanel = new SettingsPanel();
+
+    final private Properties settings = new Properties();
+    final private File settingsFile = new File("settings.properties");
 
     public HDApp() {
+        logger.info("HDApp Starting");
+        //OSManager.storeData("Helldivers2_Randomizer");
+        //OSManager.getCacheDirectory();
+        //OSManager.getConfigDirectory();
+        //OSManager.getLogDirectory();
+        loadSettings();
         initComponents();
+
     }
     private void initComponents() {
 
@@ -23,47 +45,68 @@ public class HDApp {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1042, 540);
+        setFrameIcon();
 
+        MyTabbedPane tabPanel = new MyTabbedPane(new Dimension(1042, 540));
         tabPanel.setBackground(new Color(51,51,51));
         tabPanel.setBackground(new Color(51,51,51));
         tabPanel.setForeground(Color.WHITE);
         tabPanel.setBorder(null);
 
-        MyTabbedPane tabPanelz = new MyTabbedPane(new Dimension(1042, 540));
-        tabPanelz.setBackground(new Color(51,51,51));
-        tabPanelz.setBackground(new Color(51,51,51));
-        tabPanelz.setForeground(Color.WHITE);
-        tabPanelz.setBorder(null);
+        //settingsPanel.setSettingPanelSize(frame.getPreferredSize().height,frame.getPreferredSize().width);
 
         JPanel randomizerPage = mainPanel.getMainPanel();
         JScrollPane rulePage = rulesPanel.getjScrollPane();
+        JPanel settingsPage = settingsPanel.getPanel();
 
-        tabPanelz.addTab(randomizerPage, new MyComponent("Randomizer" ));
-        tabPanelz.addTab(rulePage, new MyComponent("Randomzier Rules" ));
+        tabPanel.addTab(randomizerPage, new MyComponent("Randomizer" ));
+        tabPanel.addTab(rulePage, new MyComponent("Randomzier Rules" ));
+        //tabPanel.addTab(settingsPage,new MyComponent("Settings"));
 
-//        JPanel randomizerPage = mainPanel.getMainPanel();
-//        tabPanel.addTab("Randomizer", randomizerPage);
-//
-////        JPanel gearPage = gearPanel.getPanel();
-////        tabPanel.addTab("Gear Information", gearPage);
-//
-//        JScrollPane rulePage = rulesPanel.getjScrollPane();
-//        tabPanel.addTab("Randomzier Rules", rulePage);
-
-        frame.add(tabPanelz);
+        frame.add(tabPanel);
+        CustomCursor cursor = new CustomCursor();
+        frame.setCursor(cursor.create(CustomCursor.Type.CUSTOM_ARROW));
         frame.setVisible(true);
     }
-    private JTabbedPane makeTabPane(){
+    private void loadSettings() {
+        if (settingsFile.exists()) {
+            try (FileInputStream fis = new FileInputStream(settingsFile)) {
+                settings.load(fis);
+            } catch (IOException e) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Error IOException", e);
+                JOptionPane.showMessageDialog(null, "An error occurred loading the settings file.",
+                        "Read File Error", JOptionPane.ERROR_MESSAGE);
+                logger.log(Level.WARNING,"An error occurred loading the settings file.");
+            }
+        }
+    }
+    private void setFrameIcon() {
+        try {
+            BufferedImage icon = ImageIO.read(Objects.requireNonNull(getClass().getResource("/HD2App.png")));
 
-        //For some reason UIManager needs to be done before making the object
-        UIManager.put("TabbedPane.selected", Color.GRAY);
-        UIManager.put("TabbedPane.hoverColor", Color.YELLOW);
-        UIManager.put("TabbedPane.hoverForeground", Color.YELLOW);
-        UIManager.put("TabbedPane.selectedInsets", Color.WHITE);
-        UIManager.put("TabbedPane.tabSelectedInsets", Color.WHITE);
-        UIManager.put("TabbedPane.underlineColor", Color.WHITE);
-        UIManager.put("TabbedPane.tabSelectionHeight", 5);
-        UIManager.put("TabbedPane.cardTabSelectionHeight", 5);
-        return new JTabbedPane();
+            int targetSize = 128;
+            int w = icon.getWidth();
+            int h = icon.getHeight();
+
+            float ratio = (float) w / h;
+            int newW = ratio >= 1 ? targetSize : (int) (targetSize * ratio);
+            int newH = ratio < 1 ? targetSize : (int) (targetSize / ratio);
+
+            Image scaled = icon.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+
+            BufferedImage finalIcon = new BufferedImage(targetSize, targetSize, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = finalIcon.createGraphics();
+
+            int x = (targetSize - newW);
+            int y = (targetSize - newH) / 2;
+
+            g2.drawImage(scaled, x, y, null);
+            g2.dispose();
+
+            frame.setIconImage(finalIcon);
+
+        } catch (IOException e) {
+            logger.log(Level.WARNING,"Desktop icon did not load");
+        }
     }
 }
