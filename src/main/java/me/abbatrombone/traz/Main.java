bbb7520018c214fd433d481e45e88a2189084507
abbatrombone <redggreen@gmail.com>
2026-02-12 21:31:06 -0600

    removed merge test comment

--- src/main/java/me/abbatrombone/traz/Main.java
+++ src/main/java/me/abbatrombone/traz/Main.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEst");
         LoggerSetup.initializeLogger();
         SwingUtilities.invokeLater(HDApp::new);
 //        feel();
